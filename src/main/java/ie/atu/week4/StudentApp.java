7292c7cbd84cc98e2daaaeb56706706164061fef
daraghkearney <g00438215@atu.ie>
2024-11-06 14:44:50 +0000

    first commit

---
+++ src/main/java/ie/atu/week4/StudentApp.java
@@ -0,0 +1,12 @@
+package ie.atu.week4;
+import java.util.Scanner;
+
+public class StudentApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Student1//
+        System.out.println("Please enter your name: ");
+        String name = scanner.nextLine();
+    }
+}
